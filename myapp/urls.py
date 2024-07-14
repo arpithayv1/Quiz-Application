4fb93ad4f91fcc743418b903c78c3e3f45d16bba
ARPITHA Y V <arpithayv@jssateb.ac.in>
2024-07-14 22:51:59 +0530

    changed

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import quiz_view, quiz_success
 
 urlpatterns = [
-    path('quiz/', quiz_view, name='quiz'),
+    path('', quiz_view, name='quiz'),
     path('quiz_success/<int:score>/', quiz_success, name='quiz_success'),
 ]
